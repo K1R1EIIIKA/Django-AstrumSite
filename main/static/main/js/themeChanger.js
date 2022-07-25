beae81a8b5fddacc73874a5c76244a2768fefd46
K1R1EIIIKA <sedoikin.k@gmail.com>
2022-07-25 21:43:17 +0400

    v0.3 FINALLY working theme memorisation 😎😎😎

--- main/static/main/js/themeChanger.js
+++ main/static/main/js/themeChanger.js
@@ -1,9 +1,18 @@
-function btn() {
-    let theme = document.getElementById("theme-link");
+let activeTheme = localStorage.getItem("theme")
+let theme = document.getElementById("theme-link");
 
+function btn() {
     if (theme.getAttribute("href") === "/static/main/css/darkTheme.css") {
         theme.href = "/static/main/css/lightTheme.css";
+        localStorage.setItem("theme", "/static/main/css/lightTheme.css")
     } else {
         theme.href = "/static/main/css/darkTheme.css";
+        localStorage.setItem("theme", "/static/main/css/darkTheme.css")
     }
+}
+
+if (activeTheme === null) {
+    theme.href = "/static/main/css/lightTheme.css";
+} else {
+    theme.href = activeTheme
 }
