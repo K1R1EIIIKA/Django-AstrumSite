8dae10d16ed55dcb0b7ff1e314a0b0cbc551059f
K1R1EIIIKA <sedoikin.k@gmail.com>
2022-07-25 21:09:58 +0400

    v0.2 working theme changer but without memorisation

--- main/static/main/js/themeChanger.js
+++ main/static/main/js/themeChanger.js
@@ -1,14 +1,9 @@
-const toggle = document.getElementById("theme-button");
-const theme = window.localStorage.getItem("theme");
+function btn() {
+    let theme = document.getElementById("theme-link");
 
-/* verifica se o tema armazenado no localStorage é escuro
-se sim aplica o tema escuro ao body */
-if (theme === "dark") document.body.classList.add("dark");
-
-// event listener para quando o botão de alterar o tema for clicado
-toggle.addEventListener("click", () => {
-  document.body.classList.toggle("dark");
-  if (theme === "dark") {
-    window.localStorage.setItem("theme", "light");
-  } else window.localStorage.setItem("theme", "dark");
-});
+    if (theme.getAttribute("href") === "/static/main/css/darkTheme.css") {
+        theme.href = "/static/main/css/lightTheme.css";
+    } else {
+        theme.href = "/static/main/css/darkTheme.css";
+    }
+}
