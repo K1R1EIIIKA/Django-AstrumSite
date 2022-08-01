fbcfd95efcc529a612f6753c2199047268472ce2
K1R1EIIIKA <sedoikin.k@gmail.com>
2022-08-01 23:56:47 +0400

    v0.10 added footer

--- account/forms.py
+++ account/forms.py
@@ -1,4 +1,4 @@
-from django.forms import TextInput, EmailInput, PasswordInput
+from django.forms import TextInput, EmailInput
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
